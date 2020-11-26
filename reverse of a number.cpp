3b49cc4df4f8a5950a9e85834f810fe97ab51b2c
anushreepaul9699 <74617999+anushreepaul9699@users.noreply.github.com>
2020-11-27 01:32:43 +0530

    Add files via upload

---
+++ reverse of a number.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n,r,sum;
+	cout<<"Enter the number : "<<endl;
+	cin>>n;
+	
+	while(n>0)
+	{
+		r=n%10;
+		sum=(sum*10)+r;
+		n=n/10;
+	}
+cout<<"The reverse is"<<sum<<endl;
+	return 0;
+}
