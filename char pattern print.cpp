077d3289abf3253b503a255ce89cb520d072520b
anushreepaul9699 <74617999+anushreepaul9699@users.noreply.github.com>
2020-12-03 14:11:18 +0530

    Add files via upload

---
+++ char pattern print.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int i,j,r;
+	cout<<"Enter the number of rows you want to print ";
+	cin>>r;
+	char ch='A';
+	for(i=1;i<=r;i++)
+	{
+		
+			
+		for(j=1;j<=i;j++)
+		{
+			
+			cout<<ch;
+			ch++;
+		}
+		cout<<"\n";
+		
+	}
+	return 0;
+}
